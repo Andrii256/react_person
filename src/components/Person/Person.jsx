285e4b204cd279c4882244bcfd6d9773f014a49d
Andrii Hrushetskyi <hrushetskyi.andrii@gmail.com>
2025-03-04 21:01:06 +0100

    finish

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,15 @@
-// export const Person = ({ person }) => ();
+import './Person.scss';
+
+export const Person = ({
+  person: { name, age, sex, isMarried, partnerName },
+}) => (
+  <section className="Person">
+    <h2 className="Person__name">My name is {name}</h2>
+    {typeof age === 'number' && <p className="Person__age">I am {age}</p>}
+    <p className="Person__partner">
+      {isMarried
+        ? `${partnerName} is my ${(sex === 'm' && 'wife') || (sex === 'f' && 'husband') || 'partner'}`
+        : 'I am not married'}
+    </p>
+  </section>
+);
